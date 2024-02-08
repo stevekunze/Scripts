b4dc706eae81e2e1784aba84c50b4a619686b205
Steve Kunze <126890530+stevekunze@users.noreply.github.com>
2024-02-08 17:31:28 +0100

    Update daily_restic_cloud_backup.sh

--- restic/daily_restic_cloud_backup.sh
+++ restic/daily_restic_cloud_backup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Date: 13.01.2024 (DD-MM-YYYY)
 # Author: Steve Kunze, Ger, 15344 Strausberg
-# Discription: Restic Backup Script with max Compression enabled. Sends Backup to Hetzner Cloud Storage and checks the repository for errors. It saves the results in a file and sends those to gotify 
-# Script_Name: Daily Restic Backup   
+# Discription: restic backup script with max compression enabled. sends backup to hetzner cloud storage and checks the repository for errors. It saves the results in a file and sends those to gotify 
+# Script_Name: daily restic backup   
 
+# restic backup script with max compression enabled. sends backup to hetzner cloud storage
 restic -r sftp:hsb:Restic-Backup-Nextcloud --password-file /home/admin/restic/.restic-hetzner-file backup /mnt/truenasshare/ --compression=max > rlog.txt
-#short health check and append result in rlog.txt | if you want to run more in depth check append option --read-data
 #formatting with line breaks and dashes for better formatting
 echo >> rlog.txt
 echo '----------------' >> rlog.txt
 echo >> rlog.txt
+#short health check and append result in rlog.txt | if you want to run more in depth check append option --read-data
 restic -r sftp:hsb:Restic-Backup-Nextcloud --password-file /home/admin/restic/.restic-hetzner-file check >> rlog.txt
 #save output of rlog.txt in cariable called "result"
 result="$(cat rlog.txt)"
