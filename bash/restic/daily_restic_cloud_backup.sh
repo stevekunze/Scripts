06e1a5bf60a7fe48dda877183df49780de2c91ce
stevekunze <stevekunze@protonmail.com>
2024-02-09 12:10:16 +0100

    added packages

--- bash/restic/daily_restic_cloud_backup.sh
+++ bash/restic/daily_restic_cloud_backup.sh
@@ -16,4 +16,3 @@ restic -r sftp:hsb:Restic-Backup-Nextcloud --password-file /home/admin/restic/.r
 result="$(cat rlog.txt)"
 # Send to gotify
 curl "https://gotify.famkunze.eu/message?token=GOTIFYAPITOKEN" -F "title=Restic Cloud backup finished" -F "message=$result" -F "priority=1"
-
